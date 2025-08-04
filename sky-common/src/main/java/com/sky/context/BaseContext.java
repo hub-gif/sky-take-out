6c3432e8eaaa20781e3b378ce43209f096b952a2
hub-gif <2487812171@qq.com>
2025-08-04 12:52:48 +0800

    新增员工业务代码开发

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -1,5 +1,6 @@
 package com.sky.context;
 
+//基于ThreadLocal封装工具类，用于在多线程环境下存储和获取当前线程的用户ID,每次发起请求的线程ID都不同
 public class BaseContext {
 
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
